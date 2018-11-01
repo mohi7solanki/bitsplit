6b00bf099d4317c9e64c8929344f22a112bd9749
Karan Suthar <karansthr97@gmail.com>
2018-11-01 07:25:03 +0530

    Change admin header

--- bitsplit/settings.py
+++ bitsplit/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ADMIN_SITE_HEADER = 'BitSplit Administration'
+ADMIN_INDEX_TITLE = 'Admin'
+ADMIN_SITE_TITLE = 'BitSplit'
