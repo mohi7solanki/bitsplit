c326f440c2aa540a432d57b630d80a0a88d4c840
Mohit Solanki <mohitsolanki619@gmail.com>
2019-01-06 19:02:35 +0530

    Use callback url from settings

--- bitsplit/api/views.py
+++ bitsplit/api/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 
 from bitsplit.corpus import models
@@ -26,5 +27,5 @@ class ListBitsplits(APIView):
 class GoogleLogin(SocialLoginView):
     authentication_classes = (JSONWebTokenAuthentication,)
     adapter_class = GoogleOAuth2Adapter
-    callback_url = 'http://localhost:8000/accounts/google/login/callback/'
+    callback_url = settings.CALLBACK_URL
     client_class = OAuth2Client
